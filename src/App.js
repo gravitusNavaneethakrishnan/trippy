7980091f7850b0e028d0ca7ffb5948c10dba683b
gravitustech <gravitusnx@gmail.com>
2024-07-22 16:00:19 +0530

    22/07/2024

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/service" element={<Service/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-      </Routes> */}
-      hi
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
